0ad74b24f93f74a10248992a8c0e14631f0307bb
fardosamohamed <Fardosa.Mohamed2@seattlecolleges.edu>
2025-04-11 15:56:03 -0700

    updated 2

--- src/Calculator2.java
+++ src/Calculator2.java
@@ -5,5 +5,23 @@ public class Calculator2 {
 
     public static void main(String[] args){
 
+        System.out.println("Enter Initial Deposit e.g. 1000:");
+        double deposit = scanner.nextDouble();
+
+        System.out.println("Enter Interest Rate e.g. 1.75 (in percentage):");
+        double interestRate = scanner.nextDouble();
+
+
+        System.out.println("Enter Number of Years  e.g. 5 (in years):");
+        double numOfYears = scanner.nextDouble();
+
+
+        double endBalance  = deposit * (Math.pow((1 + (interestRate/100/365)), (365* numOfYears)));
+        double earnInterest = endBalance - deposit;
+
+        System.out.printf("Your Future Balance: %.2f\n", endBalance);
+        System.out.printf("Your Earned Interest: %.2f\n", earnInterest);
+
+
    }
 }
